b23006d605a79fa0f830ce0d1851a04ded727344
xavier <x@xavier.dev>
2024-04-08 17:35:48 +0200

    Modified house_service

--- src/services/house_service.py
+++ src/services/house_service.py
@@ -1,26 +1,14 @@
 from repositories.user_repository import user_repository 
+from repositories.house_repository import house_repository 
 
 class HouseService():
-    def __init__(self, user_repository):
+    def __init__(self, user_repository, house_repository):
         self._user_repository = user_repository
         self._user = None
 
-    def login(self, username, password):
-
-        #For now:
-        if username == "m":
-            if password == "m":
-                return True
-        return False
-               
-    def registration(self, username, password):
-        #For now:
-        if username == "m":
-            if password == "m":
-                return True
-        return False
 
 
 house_service = HouseService(
-  user_repository
+  user_repository,
+  house_repository
 )
