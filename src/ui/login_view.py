7da704901d43b4b38c17e03c9b0cf5c70b82c189
xavier <x@xavier.dev>
2024-04-08 17:41:27 +0200

    Updated login view

--- src/ui/login_view.py
+++ src/ui/login_view.py
@@ -1,66 +1,66 @@
 from tkinter import ttk, constants
-#from repositories.user_repository import user_repository
-from services.house_service import house_service
+from repositories.user_repository import user_repository
 
 class LoginView:
-    def __init__(self, root, handle_login, handle_registration_view):
+    def __init__(self, root, handle_assessment, handle_registration):
         self._root = root
-        self._handle_registration_view = handle_registration_view
-        self._handle_login = handle_login
+        self._handle_registration = handle_registration
+        self._handle_assessment = handle_assessment
         self._frame = None
         self._username_entry = None
         self._password_entry = None
         #self_
 
         self._initialize()
-
-    def pack(self):
-        pass
-        #self._frame.pack(fill=constants.X)
-
+    
     def destroy(self):
         self._frame.destroy()
     
+
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
-        heading_label = ttk.Label(master=self._root, text="Login. Use m as username and password for hardwired user")
+        self._frame.grid(row=0, column=0)  # Grid the frame within the root widget
 
-        username_label = ttk.Label(master=self._root, text="Username")
-        self._username_entry = ttk.Entry(master=self._root)
+        heading_label = ttk.Label(master=self._frame, text="Login. Use m as username and password for hardwired user")
 
-        password_label = ttk.Label(master=self._root, text="Password")
-        self._password_entry = ttk.Entry(master=self._root)
+        username_label = ttk.Label(master=self._frame, text="Username")
+        self._username_entry = ttk.Entry(master=self._frame)
 
-        button = ttk.Button(
-          master=self._root, 
+        password_label = ttk.Label(master=self._frame, text="Password")
+        self._password_entry = ttk.Entry(master=self._frame)
+
+        login_button = ttk.Button(
+          master=self._frame, 
           text="Login",
           command=lambda: self._handle_login_click()
         )
+        registration_button = ttk.Button(
+          master=self._frame, 
+          text="Registration",
+          command=lambda: self._handle_registration_click()
+        )
 
-        heading_label.grid(columnspan=2, sticky=constants.W, padx=5, pady=5)
-        username_label.grid(padx=5, pady=5)
+        heading_label.grid(row=0, column=0, columnspan=2, sticky=constants.W, padx=5, pady=5)
+        username_label.grid(row=1, column=0, padx=5, pady=5)
         self._username_entry.grid(row=1, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
-        password_label.grid(padx=5, pady=5)
+        password_label.grid(row=2, column=0, padx=5, pady=5)
         self._password_entry.grid(row=2, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
-        button.grid(columnspan=2, sticky=(constants.E, constants.W), padx=5, pady=5)
+        login_button.grid(row=3, column=0, columnspan=2, sticky=(constants.E, constants.W), padx=5, pady=5)
+        registration_button.grid(row=4, column=0, columnspan=2, sticky=(constants.E, constants.W), padx=5, pady=5)
 
-        heading_label.grid(row=0, column=0, columnspan=2, sticky=constants.W)
-        username_label.grid(row=1, column=0)
-        self._username_entry.grid(row=1, column=1, sticky=(constants.E, constants.W))
-        password_label.grid(row=2, column=0)
-        self._password_entry.grid(row=2, column=1, sticky=(constants.E, constants.W))
-
-        button.grid(row=3, column=0, columnspan=2, sticky=(constants.E, constants.W))
-
-        self._root.grid_columnconfigure(1, weight=1, minsize=300)
+        self._root.grid_columnconfigure(0, weight=1)  # Adjust column configuration for root widget
 
     def _handle_login_click(self):
         username_entry=self._username_entry.get()
         password_entry=self._password_entry.get()
         print(f"Attempt at logging in: {username_entry}")
-        val = house_service.login(username_entry, password_entry)
-        print(val)
+        if user_repository.login(username_entry, password_entry):
+            self._handle_assessment()
 
+
+    def _handle_registration_click(self):
+        print("handling registration click by login view")
+        self._handle_registration()
 '''
 def start(self):
     heading_label = ttk.Label(master=self._root, text="Login")
