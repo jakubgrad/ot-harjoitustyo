75c06e3655af68317dcb0e2069469629b3e9bf9c
xavier <x@xavier.dev>
2024-04-08 17:36:25 +0200

    Marked old database design as old

--- src/db/db_setup.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-con = sqlite3.connect("house_app.db")
-cur = con.cursor()
-
-cur.execute("CREATE TABLE users( id INTEGER PRIMARY KEY, username TEXT, password TEXT);")
-cur.execute("CREATE TABLE houses( id INTEGER, user_id INTEGER, parameters TEXT, statistics TEXT, FOREIGN KEY(user_id) REFERENCES users(id));")
-
-con.close()
