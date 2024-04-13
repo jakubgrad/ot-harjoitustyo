adb3620cac7c7573a7ff753eac17e0fef98848da
xavier <x@xavier.dev>
2024-04-13 17:23:09 +0200

    Added new tests

--- src/tests/repositories/user_repository_test.py
+++ src/tests/repositories/user_repository_test.py
@@ -2,9 +2,10 @@ import unittest
 from repositories.user_repository import user_repository
 from initialize_database import initialize_database
 
+
 class TestUserRepository(unittest.TestCase):
     def setUp(self):
-        #to implement: user_repository.delete_all()
+        # to implement: user_repository.delete_all()
         initialize_database()
         cursor = user_repository._connection.cursor()
 
@@ -15,39 +16,39 @@ class TestUserRepository(unittest.TestCase):
             VALUES
                 ("x","x")
         ''')
-    
+
     def test_user_cannot_register_under_existing_username(self):
-        result = user_repository.register("x","x")
+        result = user_repository.register("x", "x")
 
         self.assertEqual(result, False)
 
     def test_new_user_can_register_and_persits_in_database(self):
-        result = user_repository.register("newuserpersists","x")
+        result = user_repository.register("newuserpersists", "x")
         result = user_repository.check_if_username_exists("newuserpersists")
 
         self.assertEqual(result, True)
 
     def test_new_user_can_register_and_log_in_afterwards(self):
-        registration_result = user_repository.register("newuser","x")
+        registration_result = user_repository.register("newuser", "x")
         login_result = user_repository.login("newuser", "x")
 
         self.assertEqual(registration_result, True)
-        self.assertEqual(login_result, True)
+        self.assertNotEqual(login_result, False)
 
     def test_a_user_register_noone_can_take_their_name(self):
-        registration_result = user_repository.register("newuser","x")
+        registration_result = user_repository.register("newuser", "x")
         login_result = user_repository.login("newuser", "x")
 
-        another_registration_result = user_repository.register("newuser","y")
+        another_registration_result = user_repository.register("newuser", "y")
 
         self.assertEqual(registration_result, True)
-        self.assertEqual(login_result, True)
-        self.assertEqual(another_registration_result , False)
-        
+        self.assertNotEqual(login_result, False)
+        self.assertEqual(another_registration_result, False)
+
     def test_if_nonexistent_user_can_log_in(self):
         result = user_repository.login(
-                "notexistingusername",
-                "somepassword"
+            "notexistingusername",
+            "somepassword"
         )
 
         self.assertEqual(result, False)
@@ -58,8 +59,7 @@ class TestUserRepository(unittest.TestCase):
         self.assertEqual(result, True)
 
     def test_a_nonexisting_user_cannot_be_fetched(self):
-        result = user_repository.check_if_username_exists("nonexistingusername")
+        result = user_repository.check_if_username_exists(
+            "nonexistingusername")
 
         self.assertEqual(result, False)
-
-
--- src/tests/services/house_service_test.py
+++ src/tests/services/house_service_test.py
@@ -1,19 +1,36 @@
 import unittest
 from services.house_service import HouseService
-from repositories.user_repository import user_repository 
-from repositories.house_repository import house_repository 
+from repositories.user_repository import user_repository
+from repositories.house_repository import house_repository
+
 
 class TestHouseService(unittest.TestCase):
     def setUp(self):
         self.house_service = HouseService(
-          user_repository,
-          house_repository
+            user_repository,
+            house_repository
         )
-        #would be nice to have:
-        #todo_repository.delete_all()
-        #user_repository.delete_all()
+        result = self.house_service.register("user1", "pass1")
+        # would be nice to have:
+        # todo_repository.delete_all()
+        # user_repository.delete_all()
 
     def test_one_equals_one(self):
-
         self.assertEqual(1, 1)
+
+    def test_registering_with_new_username_works(self):
+        result = self.house_service.register("new_username", "password")
+        self.assertEqual(result, True)
+
+    def test_registering_with_existing_username_does_not_work(self):
+        result = self.house_service.register("user1", "pass1")
+        self.assertEqual(result, False)
+
+    def test_login_with_existing_username_works(self):
+        result = self.house_service.login("user1", "pass1")
+        self.assertNotEqual(result, False)
+
+    def test_login_with_nonexisting_username_works(self):
+        result = self.house_service.login("non_existing_user", "pass")
+        self.assertEqual(result, False)
 
