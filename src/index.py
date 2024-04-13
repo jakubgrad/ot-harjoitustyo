9a999a1ddb9aa86a834f8b67a1ecd2f9de6edad2
xavier <x@xavier.dev>
2024-04-13 17:20:10 +0200

    Removed unnecessary dependencies

--- src/index.py
+++ src/index.py
@@ -1,9 +1,11 @@
 from tkinter import Tk
-from ui.ui import UI 
+from ui.ui import UI
 
 from config import DATABASE_PATH
 
 print(DATABASE_PATH)
+
+
 def main():
     window = Tk()
     window.title("House app")
