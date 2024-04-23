de0f4a4b2cffb6b6dc15af417171748f109669a3
xavier <x@xavier.dev>
2024-04-23 15:41:03 +0200

    Added dictionary fields to the model for easier user:

--- src/entities/model.py
+++ src/entities/model.py
@@ -1,4 +1,6 @@
 class Model:
-    def __init__(self,  house_age,  type_of_heating):
+    def __init__(self,  house_age,  types_of_heating,  house_age_as_dictionary, types_of_heating_as_dictionary):
         self._house_age = house_age
-        self._type_of_heating = type_of_heating
+        self._types_of_heating = types_of_heating
+        self._house_age_as_dictionary =  house_age_as_dictionary
+        self._types_of_heating_as_dictionary = types_of_heating_as_dictionary  
