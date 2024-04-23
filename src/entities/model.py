915c742c84f20ce82f3c0d1bc057d4344d2c70f3
xavier <x@xavier.dev>
2024-04-23 17:00:02 +0200

    Turned dictionary attributes into an access method to comply with pylint:

--- src/entities/model.py
+++ src/entities/model.py
@@ -1,6 +1,16 @@
 class Model:
-    def __init__(self,  house_age,  types_of_heating,  house_age_as_dictionary, types_of_heating_as_dictionary):
+    def __init__(self,  house_age,  types_of_heating):
         self._house_age = house_age
         self._types_of_heating = types_of_heating
-        self._house_age_as_dictionary =  house_age_as_dictionary
-        self._types_of_heating_as_dictionary = types_of_heating_as_dictionary  
+
+    def get_types_of_heating_as_dictionary(self):
+        t = {}
+        for i, row in enumerate(self._types_of_heating):
+            t[i+1] = {"energy_consumption": row[3], "pollution": row[4]}
+        return t
+
+    def get_house_age_as_dictionary(self):
+        h = {}
+        for i, row in enumerate(self._house_age):
+            h[row[1]] = {"energy_consumption": row[3], "pollution": row[4]}
+        return h
