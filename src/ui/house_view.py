0344e02d5c47fe39736435ee16e7bbbdf05177db
xavier <x@xavier.dev>
2024-04-23 15:31:32 +0200

    Added image for nicer viewing experience:

--- src/ui/house_view.py
+++ src/ui/house_view.py
@@ -1,6 +1,8 @@
 from tkinter import ttk, constants
 import tkinter as tk
 from services.house_service import house_service
+from config import HOUSE_IMAGE_PATH
+from PIL import Image, ImageTk
 
 
 class HouseView:
@@ -15,6 +17,7 @@ class HouseView:
         self._frame = None
         self._house_age_entry = None
         self._type_of_heating_entry = None
+        self.image_path =  HOUSE_IMAGE_PATH
 
         self._initialize()
 
@@ -76,8 +79,12 @@ class HouseView:
         user_id_label.grid(row=6, column=0, columnspan=2, padx=5, pady=5)
         pollution_label.grid(row=2, column=0, padx=5, pady=5)
 
-        # Adjust column configuration for root widget
-        self._root.grid_columnconfigure(0, weight=1)
+        
+        image = Image.open(self.image_path)
+        resized_image = image.resize((200, 200))  
+        self.photo = ImageTk.PhotoImage(resized_image)
+        image_label = tk.Label(master=self._frame, image=self.photo)
+        image_label.grid(row=7, column=0, rowspan=6, padx=5, pady=5)
 
     def _handle_logout_click(self):
         self._handle_login()
