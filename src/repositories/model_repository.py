df1ab63af3fc1037ee82c1baed8dceb4eb1b8050
xavier <x@xavier.dev>
2024-04-23 15:39:18 +0200

    Added loading a model from database functionality and checkig for correctness:

--- src/repositories/model_repository.py
+++ src/repositories/model_repository.py
@@ -1,22 +1,72 @@
 from database_connection import get_database_connection
-from entities.house import Model
+from entities.model import Model
+from entities.house import House
+from repositories.house_repository import house_repository
 
 
 class ModelRepository():
     def __init__(self, connection):
         self._connection = connection
 
-    def give_parameters(self)
-        if not self.check_if_parameters_exist():
-            print("Parameters don't exist")
+
+    def evaluate_equation_for_house(self, house):
+        try:
+            house_age, type_of_heating = house_repository.unpack_parameters(house._parameters)
+            house_age = self.find_min_year(house_age)
+            model = self.get_model()
+            energy_consumption = model._types_of_heating_as_dictionary[type_of_heating]["energy_consumption"] + model._house_age_as_dictionary[house_age]["energy_consumption"]
+            pollution = model._types_of_heating_as_dictionary[type_of_heating]["pollution"] + model._house_age_as_dictionary[house_age]["pollution"]
+
+            return energy_consumption, pollution
+        except:
+            print(" evaluate_equation_for_house failed")
             return False
+
+    def check_equation(self, equation):
+        model = self.get_model()
+
+        parameters = "1991" + ","+"2"
+        example_house = House(1, parameters)
+        try:
+            print(self.evaluate_equation_for_house(example_house))
+        except:
+            print("check_equation failed")
+            return False
+        
+        return True
+        
+    def find_min_year(self,year):
+        year = int(year)
+        cursor = self._connection.cursor()
+        print("Attempts to get parameters from db")
+
+        cursor.execute('''
+            SELECT 
+                year_min 
+            FROM
+                house_age
+            WHERE 
+                year_min <= ?
+            AND
+                year_max > ?
+        ''', (year,year))
+
+        result = cursor.fetchone()
+        print(f"min year found: {result}")
+        if result:
+            return result[0]
+
+    def get_model(self):
+        #if not self.check_if_parameters_exist():
+        #    print("Parameters don't exist")
+        #    return False
 
         cursor = self._connection.cursor()
         #data = (username, password)
         print("Attempts to get parameters from db")
 
         cursor.execute('''
-            SELECT (
+            SELECT 
                 id, 
                 year_min, 
                 year_max,
@@ -24,21 +74,41 @@ class ModelRepository():
                 pollution
             FROM
                 house_age
-        ''', data)
+        ''')
 
-        result = cursor.fetchall()
+        result_house_age = cursor.fetchall()
 
-        print(f"result of login query: {result}")
+        print(f"result of house_age query: {result_house_age}")
+        cursor.execute('''
+            SELECT 
+            id,
+            type,
+            name,
+            energy_consumption,
+            pollution
+            FROM
+                types_of_heating
+        ''')
 
-        #user_id = result['id']
-        #username = result['username']
-        #print("User object:")
-        #print(f"user_user_id:{id},username:{username}")
-        #user = User(user_id, username)
-        if result:
-            return True
+        result_types_of_heating = cursor.fetchall()
+
+        print(f"result of types_of_heating query: {result_types_of_heating}")
+
+        t = {}
+        h = {}
+        for i, row in enumerate(result_types_of_heating  ):
+            t[i+1]={"energy_consumption":row[3],"pollution":row[4]}
+
+        for i, row in enumerate(result_house_age ):
+            h[row[1]]={"energy_consumption":row[3],"pollution":row[4]}
+
+        house_age_as_dictionary = h
+        types_of_heating_as_dictionary = t
+
+        if result_house_age and result_types_of_heating:
+            return Model(result_house_age, result_types_of_heating, house_age_as_dictionary, types_of_heating_as_dictionary  )
         return False
 
 
     
-model_repository = ModelRepository(get_database_connection(), model)
+model_repository = ModelRepository(get_database_connection())
