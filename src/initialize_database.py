f2259644f9b57740ece96280b662a30cc2b4cc73
xavier <x@xavier.dev>
2024-04-23 13:46:08 +0200

    Added administrator and model parameters tables:

--- src/initialize_database.py
+++ src/initialize_database.py
@@ -12,6 +12,16 @@ def drop_tables(connection):
         drop table if exists houses;
     ''')
 
+    cursor.execute('''
+        drop table if exists administrators;
+    ''')
+    cursor.execute('''
+        drop table if exists types_of_heating;
+    ''')
+    cursor.execute('''
+        drop table if exists house_age;
+    ''')
+
     connection.commit()
 
 
@@ -34,6 +44,80 @@ def create_tables(connection):
             FOREIGN KEY(user_id) REFERENCES users(id)   
         );
     ''')
+
+    cursor.execute('''
+        CREATE TABLE administrators ( 
+            id INTEGER PRIMARY KEY,
+            username TEXT,
+            password TEXT
+        );
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE types_of_heating ( 
+            id INTEGER PRIMARY KEY,
+            type INTEGER,
+            name TEXT,
+            energy_consumption INTEGER,
+            pollution INTEGER
+        );
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE house_age ( 
+            id INTEGER PRIMARY KEY,
+            year_min INTEGER,
+            year_max INTEGER,
+            energy_consumption INTEGER,
+            pollution INTEGER
+        );
+    ''')
+
+
+    cursor.execute('''
+        INSERT INTO administrators ( 
+            username,
+            password)
+        VALUES (
+            'm',
+            'm'
+        );
+    ''')
+
+    cursor.execute('''
+        INSERT INTO house_age ( 
+            year_min,
+            year_max,
+            energy_consumption,
+            pollution )
+        VALUES 
+            (1900, 1989, 10, 10),
+            (1990, 2000, 5, 7),
+            (2001, 2010, 4, 6),  
+            (2011, 2020, 3, 5),  
+            (2021, 2030, 2, 4);  
+    ''')
+
+    cursor.execute('''
+        INSERT INTO types_of_heating ( 
+            type,
+            name,
+            energy_consumption,
+            pollution )
+        VALUES 
+            (1, "Heat pump", 10, 10),
+            (2, "Gas boiler", 8, 7),
+            (3, "Electric heater", 12, 12),
+            (4, "Wood stove", 6, 9),
+            (5, "Solar panels", 3, 2),
+            (6, "District heating", 9, 8),
+            (7, "Oil boiler", 9, 10),
+            (8, "Pellet stove", 7, 5),
+            (9, "Geothermal heating", 5, 4);
+    ''')
+
+
+
     connection.commit()
 
 
