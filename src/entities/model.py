ebe3574ac409468ede4b770e36169d66e4336346
xavier <x@xavier.dev>
2024-04-23 17:53:59 +0200

    Fixed whitespace

--- src/entities/model.py
+++ src/entities/model.py
@@ -1,5 +1,5 @@
 class Model:
-    def __init__(self,  house_age,  types_of_heating):
+    def __init__(self,  house_age, types_of_heating):
         self._house_age = house_age
         self._types_of_heating = types_of_heating
 
