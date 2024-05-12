43964f02232c80fe3297af527c6e266a9872f3db
xavier <x@xavier.dev>
2024-05-12 15:28:56 +0200

    Added update_house_age, update_types_of_heating for updating the model, deleted unnecessary printouts and added docstrings:

--- src/repositories/model_repository.py
+++ src/repositories/model_repository.py
@@ -1,7 +1,5 @@
-import ast
 from database_connection import get_database_connection
 from entities.model import Model
-from entities.house import House
 
 class ModelRepository:
     """Repository class for handling model data and evaluation.
@@ -22,15 +20,22 @@ class ModelRepository:
         self._connection = connection
 
     def calculate_pollution(self,parameters):
-        p1,p2 = parameters
+       """Calculates pollution based on given parameters.
+
+        Args:
+            parameters (tuple): A tuple containing year and type of heating.
+
+        Returns:
+            int: The calculated pollution value.
+        """
+
+        year,type_of_heating = parameters
+        year = self.find_min_year(year)
+        if not year:
+            #means that the house is older than there is data for
+            year = self.find_min_year(2021)
+
         cursor = self._connection.cursor()
-
-        year = self.find_min_year(p1)
-        # type of heating
-        print(f"year after find_mind_year: {year}")
-        type_of_heating = p2
-
-        print("Attempts to calculate pollution from parameters from db")
 
         cursor.execute('''
             SELECT 
@@ -42,7 +47,6 @@ class ModelRepository:
         ''', (year,))
 
         house_age_pollution = cursor.fetchone()["pollution"]
-        print(f"houes age pollution found: {house_age_pollution }")
 
         cursor.execute('''
             SELECT 
@@ -54,20 +58,25 @@ class ModelRepository:
         ''', (type_of_heating,))
 
         type_of_heating_pollution = cursor.fetchone()["pollution"]
-        print(f"type_of_heating_pollution: {type_of_heating_pollution }")
-        return type_of_heating_pollution + house_age_pollution 
+
+        return type_of_heating_pollution + house_age_pollution
 
     def calculate_consumption(self,parameters):
-        p1,p2 = parameters
+        """Calculates energy consumption based on given parameters.
+
+        Args:
+            parameters (tuple): A tuple containing year and type of heating.
+
+        Returns:
+            int: The calculated energy consumption value.
+        """
+
+        year,type_of_heating  = parameters
+
+        if not year:
+            year = self.find_min_year(2021)
 
         cursor = self._connection.cursor()
-
-        year = self.find_min_year(p1)
-        # type of heating
-        print(f"year after find_mind_year: {year}")
-        type_of_heating = p2
-
-        print("Attempts to calculate pollution from parameters from db")
 
         cursor.execute('''
             SELECT 
@@ -79,7 +88,6 @@ class ModelRepository:
         ''', (year,))
 
         house_age_consumption = cursor.fetchone()["energy_consumption"]
-        print(f"houes age consumption found: {house_age_consumption }")
 
         cursor.execute('''
             SELECT 
@@ -91,8 +99,8 @@ class ModelRepository:
         ''', (type_of_heating,))
 
         type_of_heating_consumption = cursor.fetchone()["energy_consumption"]
-        print(f"type_of_heating_consumption: {type_of_heating_consumption }")
-        return type_of_heating_consumption + house_age_consumption 
+
+        return type_of_heating_consumption + house_age_consumption
 
     def find_min_year(self, year):
         """Finds the minimum year from the database.
@@ -119,7 +127,7 @@ class ModelRepository:
         ''', (year, year))
 
         result = cursor.fetchone()
-        print(f"min year found: {result}")
+
         if result:
             return result[0]
         return False
@@ -130,8 +138,8 @@ class ModelRepository:
         Returns:
             Model object containing data from the database.
         """
+
         cursor = self._connection.cursor()
-        print("Attempts to get parameters from db")
 
         cursor.execute('''
             SELECT 
@@ -142,78 +150,82 @@ class ModelRepository:
                 pollution
             FROM
                 house_age
+            ORDER BY
+                year_min ASC;
         ''')
 
         result_house_age = cursor.fetchall()
 
-        print(f"result of house_age query: {result_house_age}")
         cursor.execute('''
             SELECT 
-            id,
-            type,
-            name,
-            energy_consumption,
-            pollution
+                id,
+                type,
+                name,
+                energy_consumption,
+                pollution
             FROM
                 types_of_heating
+            ORDER BY
+                type ASC;
         ''')
 
         result_types_of_heating = cursor.fetchall()
-
-        print(f"result of types_of_heating query: {result_types_of_heating}")
 
         if result_house_age and result_types_of_heating:
             return Model(result_house_age, result_types_of_heating)
         return False
 
-    def update_model(self, info):
-        """Updates the model in the database.
+    def update_types_of_heating(self, new_toh):
+        """Updates the types_of_heating table in the database.
 
         Args:
-            info: Information to be updated.
+            new_toh: Information to be updated.
 
         Returns:
             True if the update is successful, False otherwise.
         """
         cursor = self._connection.cursor()
 
-        if "min_year" in info:
-            min_year = info["min_year"]
-            max_year = info["max_year"]
-            ha_energy_consumption = info["ha_energy_consumption"]
-            ha_pollution = info["ha_pollution"]
+        data = tuple(new_toh.values())
 
-            data = (min_year, max_year, ha_energy_consumption, ha_pollution)
+        cursor.execute('''
+            REPLACE INTO types_of_heating(
+                type,
+                name,
+                energy_consumption,
+                pollution)
+            VALUES
+                (?,?,?,?)
+        ''', data)
+        self._connection.commit()
 
-            cursor.execute('''
-                INSERT INTO house_age(
-                    year_min ,
-                    year_max,
-                    energy_consumption,
-                    pollution)
-                VALUES
-                    (?,?,?,?)
-            ''', data)
-            self._connection.commit()
-            print("house_age updated")
-        else:
-            type_of_heating = info["type_of_heating"]
-            name = info["name"]
-            energy_consumption = info["energy_consumption"]
-            pollution = info["pollution"]
+        return True
 
-            data = (type_of_heating, name, energy_consumption, pollution)
+    def update_house_age(self, new_house_age):
+        """Updates the house age information in the database.
 
-            cursor.execute('''
-                INSERT INTO types_of_heating(
-                    type,
-                    name,
-                    energy_consumption,
-                    pollution)
-                VALUES
-                    (?,?,?,?)
-            ''', data)
-            self._connection.commit()
+        Args:
+            new_house_age (dict): Dictionary containing the new house age information.
+
+        Returns:
+            bool: True if the update is successful, False otherwise.
+        """
+
+        cursor = self._connection.cursor()
+
+        data = tuple(new_house_age.values())
+        print(data)
+
+        cursor.execute('''
+            REPLACE INTO house_age(
+                year_min ,
+                year_max,
+                energy_consumption,
+                pollution)
+            VALUES
+                (?,?,?,?)
+        ''', data)
+        self._connection.commit()
 
         return True
 
