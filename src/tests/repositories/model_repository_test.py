627c9cefc5eee467ff06d5a2d2b2cda8fe0d7e52
xavier <x@xavier.dev>
2024-05-12 18:54:42 +0200

    Added tests for calculating pollution and energy consumption:

--- src/tests/repositories/model_repository_test.py
+++ src/tests/repositories/model_repository_test.py
@@ -19,16 +19,39 @@ class TestModelRepository(unittest.TestCase):
 
         self.assertIsNotNone(result)
 
-    def test_update_model(self):
-        info = {
+    def test_update_house_age(self):
+        new_house_age = {
             "min_year": 1990,
             "max_year": 2000,
             "ha_energy_consumption": 50,
             "ha_pollution": 10
         }
-        result = model_repository.update_model(info)
+        result = model_repository.update_house_age(new_house_age)
 
         self.assertTrue(result)
+
+    def test_update_types_of_heating(self):
+        new_toh = {
+            "type_of_heating": 2,
+            "name": "Boilering",
+            "energy_consumption": 3,
+            "pollution": 5 
+        }
+        result = model_repository.update_house_age(new_toh)
+
+        self.assertTrue(result)
+
+    def test_calculating_pollution_works(self):
+        parameters = (2021, 4)
+        result = model_repository.calculate_pollution(parameters)
+
+        self.assertIsInstance(result, (int, float))
+
+    def test_calculating_energy_consumption_works(self):
+        parameters = (2021, 4)
+        result = model_repository.calculate_consumption(parameters)
+
+        self.assertIsInstance(result, (int, float))
 
 
 
